75bb7c394afbba79ef115fdfbb21e0bd854e026b
Pedro2804 <pedro28hdez@hotmail.com>
2024-05-25 12:13:06 -0600

    Agregando métodos de agregación de nodos.

--- ListaLigada/src/App.java
+++ ListaLigada/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ListaLigada lista = new ListaLigada();
+        lista.ingresarNodoFinalLista(5);
+        lista.ingresarNodoFinalLista(4);
+        lista.ingresarNodoFinalLista(3);
+        lista.ingresarNodoFinalLista(2);
+        lista.ingresarNodoFinalLista(1);
+        System.out.println("El tamaño de la lista: " + lista.tamañoLista());
+        System.out.println("La lista está vacía: " + lista.listaVacia());
+        System.out.println(lista.imprimirLista());
     }
 }
--- ListaLigada/src/ListaLigada.java
+++ ListaLigada/src/ListaLigada.java
@@ -1,3 +1,51 @@
 public class ListaLigada {
-    
+    private Nodo inicioLista;
+    private Nodo finalLista;
+    private int tamaño;
+    private String lista;
+
+    public ListaLigada(){
+        this.inicioLista =  null;
+        this.finalLista = null;
+        this.tamaño = 0;
+        this.lista = "";
+    }
+
+    public boolean listaVacia(){
+        return inicioLista == null;
+    }
+
+    public int tamañoLista(){
+        return tamaño;
+    } 
+
+    public String imprimirLista(){
+        Nodo recorrido = inicioLista;
+        lista = "";
+        while(recorrido != null){
+            lista += recorrido.informacion + " - > ";
+            recorrido = recorrido.siguiente; 
+        }
+        return lista;
+    }
+
+    public void ingresarNodoFinalLista(Object informacion){
+        Nodo nuevoNodo = new Nodo(informacion);
+        if(listaVacia()){
+            inicioLista = nuevoNodo;
+        }
+        else{
+            finalLista.siguiente = nuevoNodo;
+        }
+        finalLista = nuevoNodo;
+        tamaño++;
+    }
+
+    public void ingresarNodoInicioLista(Object informacion){
+        Nodo nuevoNodo = new Nodo(informacion);
+        nuevoNodo.siguiente = inicioLista;
+        inicioLista = nuevoNodo;
+        tamaño++;
+    }
 }
+
