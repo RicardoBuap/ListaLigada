a74c57c013ff7e24830eb2c2eac3f3dd1950bf36
Pedro2804 <pedro28hdez@hotmail.com>
2024-05-25 10:09:18 -0600

    Creación de la clase Nodo

---
+++ ListaLigada/src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo{
+    Object informacion;
+    Nodo siguiente;
+
+    public Nodo(Object informacion){
+        this.informacion = informacion;
+        this.siguiente = null;
+    }
+}
