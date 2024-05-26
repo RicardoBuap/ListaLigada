1c6cd9601022e8620fb744f52a53bab932e18122
Pedro2804 <pedro28hdez@hotmail.com>
2024-05-25 23:31:58 -0600

    Agregando el menú para la Lista Ligada.

--- ListaLigada/src/App.java
+++ ListaLigada/src/App.java
@@ -1,17 +1,105 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         ListaLigada lista = new ListaLigada();
-        lista.ingresarNodoFinalLista(1);
-        lista.ingresarNodoFinalLista(2);
-        lista.ingresarNodoFinalLista(3);
-        lista.ingresarNodoFinalLista(4);
-        lista.ingresarNodoFinalLista(5);
-        System.out.println("El tamaño de la lista: " + lista.tamañoLista());
-        System.out.println("La lista está vacía: " + lista.listaVacia());
-        System.out.println("El inicio de la lista es: " + lista.inicioLista());
-        System.out.println("El final de la lista es: " + lista.finalLista());
-        System.out.println(lista.imprimirLista());
-        lista.ingresarNodoPosicionLista("Hello World!", 6);
-        System.out.println(lista.imprimirLista());
+        Scanner teclado = new Scanner(System.in);
+        boolean continuar = true;
+        int opcion;
+        int posicion;
+        Object informacion;
+        while(continuar){
+            System.out.println("- - - - - - - - Menú - - - - - - - -");
+            System.out.println("Ingresar nodo al inicio de la lista:   1");
+            System.out.println("Ingresar nodo al final de la lista:    2");
+            System.out.println("Ingresar nodo en posición de la lista: 3");
+            System.out.println("Imprimir lista ligada: 4");
+            System.out.println("Imprimir inicio de la lista: 5");
+            System.out.println("Imprimir final de la lista:  6");
+            System.out.println("Tamaño de la lista ligada: 7");
+            System.out.println("¿La lista ligada está vacía?: 8");
+            System.out.println("Salir del programa: 9");
+            System.out.print("\nIngresa una opción del menú:");
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingresa un valor para el nodo: ");
+                    informacion = teclado.nextLine();
+                    informacion = teclado.nextLine();
+                    lista.ingresarNodoInicioLista(informacion);
+                    break;
+
+                case 2:
+                    System.out.print("Ingresa un valor para el nodo: ");
+                    informacion = teclado.nextLine();
+                    informacion = teclado.nextLine();
+                    lista.ingresarNodoFinalLista(informacion);
+                    break;
+
+                case 3:
+                    System.out.print("Ingresa la posición a la que deseas insertar el nodo: ");
+                    posicion = teclado.nextInt();
+                    System.out.print("Ingresa un valor para el nodo: ");
+                    informacion = teclado.nextLine();
+                    informacion = teclado.nextLine();
+                    lista.ingresarNodoPosicionLista(informacion, posicion);
+                    break;
+
+                case 4: 
+                    if(lista.listaVacia()){
+                        System.out.println("La lista ligada está vacía.");
+                    }
+                    else{
+                        System.out.println(lista.imprimirLista());
+                    }
+                    break;
+
+                case 5:
+                    if(lista.listaVacia()){
+                        System.out.println("La lista ligada está vacía.");
+                    }
+                    else{
+                        System.out.println("Inicio de la lista: " + lista.inicioLista());
+                    }
+                    break;
+
+                case 6:
+                    if(lista.listaVacia()){
+                        System.out.println("La lista ligada está vacía.");
+                    }
+                    else{
+                        System.out.println("Final de la lista: " + lista.finalLista());
+                    }
+                    break;
+
+                case 7:
+                    if(lista.listaVacia()){
+                        System.out.println("La lista ligada está vacía.");
+                    }
+                    else{
+                        System.out.println("Tamaño de la lista: " + lista.tamañoLista());
+                    }
+                    break;
+
+                case 8:
+                    if(lista.listaVacia()){
+                        System.out.println("La lista ligada está vacía.");
+                    }
+                    else{
+                        System.out.println("La lista ligada no está vacía.");
+                    }
+                    break;
+
+                case 9:
+                    System.out.println("Saliendo del programa...");
+                    continuar = false;
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        teclado.close();
     }
 }
